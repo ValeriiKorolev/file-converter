c450ca33bb59351792c556d487cf1abe08f1175a
ValeriiKorolev <kingvv63@gmail.com>
2022-01-17 23:26:37 +0300

    Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,30 +6,46 @@ import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+//import javax.lang.model.element.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException, ParserConfigurationException, SAXException {
 
         //Task1
         // Конвертируем строки csv в объекты класса Employee
-        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        list.forEach(System.out::println);
-
+        //String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        //String fileName = "data.csv";
+        //List<Employee> list = parseCSV(columnMapping, fileName);
+        //list.forEach(System.out::println);
         // Конвертируем объекты класса Employee строку формата json
+        //String json = listToJson(list);
+        //System.out.println(json);
+        // Записываем в файл json
+        //writeString(json, "data.json");
+
+        //Task2
+        // Конвертируем xml в объекты класса Employee
+        String fileName = "data.xml";
+        List<Employee> list = parseXML(fileName);
+        list.forEach(System.out::println);
+        //Конвертируем объекты класса Employee строку формата json
         String json = listToJson(list);
         System.out.println(json);
-
         // Записываем в файл json
-        writeString(json);
+        writeString(json, "data1.json");
 
     }
 
@@ -49,6 +65,50 @@ public class Main {
         return staff;
     }
 
+    public static List<Employee> parseXML(String fileName) throws IOException, SAXException, ParserConfigurationException {
+        List<Employee> list = new ArrayList<Employee>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+        Node node = doc.getDocumentElement();
+
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (node_.getNodeName().equals("employee")) {
+                NodeList nodeList1 = node_.getChildNodes();
+                long id =0;
+                String firstName = null;
+                String lastName = null;
+                String country = null;
+                int age = 0;
+                for (int j = 0; j < nodeList1.getLength(); j++) {
+                    Node node_1 = nodeList1.item(j);
+                    String attrName = node_1.getNodeName();
+                    String attrValue = node_1.getTextContent();
+                    switch (attrName) {
+                        case "id" :
+                            id = Long.parseLong(attrValue);
+                            break;
+                        case "firstName" :
+                            firstName = attrValue;
+                            break;
+                        case "lastName" :
+                            lastName = attrValue;
+                            break;
+                        case "country" :
+                            country = attrValue;
+                            break;
+                        case "age" :
+                            age = Integer.parseInt(attrValue);
+                    }
+                }
+                list.add(new Employee(id, firstName, lastName, country, age));
+            }
+        }
+        return list;
+    }
+
     public static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
@@ -57,8 +117,8 @@ public class Main {
         return json;
     }
 
-    public static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
             file.flush();
         }
